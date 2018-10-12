41bc9dde3f7ce80813181c5c85f8d5b1aeeaff6c
Alex <climberfields@gmail.com>
2018-10-12 14:51:51 -0500

    added packages to server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+
+    
+
+
+app.listen(3000, (req, res) => {
+    console.log('andre 3000 listening')
+})
